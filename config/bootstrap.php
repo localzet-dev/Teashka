a6cc1531f42c1d77099ade00de12313509e0d256
Ivan Zorin <creator@localzet.com>
2023-11-15 00:01:34 +0300

    Create bootstrap.php

---
+++ config/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package     Triangle Web
+ * @link        https://github.com/Triangle-org
+ *
+ * @copyright   2018-2023 UniT Group
+ * @license     https://mit-license.org MIT
+ */
+
+return [
+    Triangle\Engine\Bootstrap\Eloquent::class,
+    Triangle\Engine\Bootstrap\Session::class,
+];

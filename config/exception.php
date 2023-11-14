f5ac2a95a3cf451760a4dca38456a9b2fb06e7b5
Ivan Zorin <creator@localzet.com>
2023-11-15 00:08:43 +0300

    Create exception.php

---
+++ config/exception.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package     Triangle Web
+ * @link        https://github.com/Triangle-org
+ *
+ * @copyright   2018-2023 UniT Group
+ * @license     https://mit-license.org MIT
+ */
+
+use Triangle\Engine\Exception\ExceptionHandler;
+
+return [
+    '' => ExceptionHandler::class,
+];

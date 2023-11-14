bf0ff3bb0425f616f2ccad414cb5ce5b9a867531
Ivan Zorin <creator@localzet.com>
2023-11-15 00:01:31 +0300

    Create autoload.php

---
+++ config/autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package     Triangle Web
+ * @link        https://github.com/Triangle-org
+ *
+ * @copyright   2018-2023 UniT Group
+ * @license     https://mit-license.org MIT
+ */
+
+return [
+    'files' => [
+        base_path() . '/support/Request.php',
+        base_path() . '/support/Response.php',
+    ]
+];

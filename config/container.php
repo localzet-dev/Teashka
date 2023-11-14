a5494479f7f005951dc2854684aa633d923c48dd
Ivan Zorin <creator@localzet.com>
2023-11-15 00:01:38 +0300

    Create container.php

---
+++ config/container.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @package     Triangle Web
+ * @link        https://github.com/Triangle-org
+ *
+ * @copyright   2018-2023 Localzet Group
+ * @license     https://mit-license.org MIT
+ */
+
+// Простой контейнер
+// return new Triangle\Engine\Container;
+
+// Подключаем сервисы
+$builder = new \DI\ContainerBuilder();
+$builder->addDefinitions(config('dependence', []));
+$builder->useAutowiring(true);
+$builder->useAnnotations(true);
+return $builder->build();

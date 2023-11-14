1fd81b101d4f2433dd60a68119583f0d26b13673
Ivan Zorin <creator@localzet.com>
2023-11-15 00:09:31 +0300

    Create static.php

---
+++ config/static.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package     Triangle Web
+ * @link        https://github.com/Triangle-org
+ *
+ * @copyright   2018-2023 Localzet Group
+ * @license     https://mit-license.org MIT
+ */
+
+return [
+    'enable' => false,
+    'middleware' => [],
+];

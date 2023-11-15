8b2bd454d7411b067ba23be8114beb0e830d0e87
Ivan Zorin <creator@localzet.com>
2023-11-15 16:31:41 +0300

    Update Voice.php

--- app/helpers/Voice.php
+++ app/helpers/Voice.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace app\service;
+namespace app\helpers;
 
 use Exception;
 
 class Voice
 {
+    private static string $model = '/resources/vosk-model-small';
+    private static string $recognizer = '/resources/scripts/recognizer.py';
+
     /**
      * Распознает голосовой файл с помощью скрипта.
      *
@@ -15,16 +18,13 @@ class Voice
      */
     public static function recognize(string $voiceFile): string
     {
-        // Путь к модели
-        $modelPath = base_path() . '/resources/vosk-model-small';
-
         // Проверяем, существуют ли файлы
-        if (!file_exists($voiceFile) || !file_exists($modelPath)) {
+        if (!file_exists($voiceFile) || !file_exists(base_path(self::$model)) || !file_exists(base_path(self::$recognizer))) {
             throw new Exception('Файл не найден: ' . $voiceFile);
         }
 
         // Формируем команду для запуска скрипта распознавания голоса
-        $command = sprintf('python3 %s %s %s', base_path() . '/resources/scripts/recognizer.py', escapeshellarg($voiceFile), escapeshellarg($modelPath));
+        $command = sprintf('python3 %s %s %s', escapeshellarg(base_path(self::$recognizer)), escapeshellarg($voiceFile), escapeshellarg(base_path(self::$model)));
 
         // Запускаем команду и получаем результат
         $result = shell_exec($command);

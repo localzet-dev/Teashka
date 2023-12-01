94ef2f2fb317230d839ecd6dddd3084933b4fb03
Ivan Zorin <creator@localzet.com>
2023-12-01 14:11:40 +0300

    Update dirs.php

--- support/helpers/dirs.php
+++ support/helpers/dirs.php
@@ -13,7 +13,7 @@ function copy_dir(string $source, string $dest, bool $overwrite = false): void
         if (!is_dir($dest)) {
             mkdir($dest);
         }
-        $files = array_slice(scandir($source, SCANDIR_SORT_NONE), 2);
+        $files = array_diff(scandir($source), ['.', '..']) ?: [];
         foreach ($files as $file) {
             copy_dir("$source/$file", "$dest/$file");
         }
@@ -33,7 +33,7 @@ function scan_dir(string $basePath, bool $withBasePath = true): array
     if (!is_dir($basePath)) {
         return [];
     }
-    $paths = array_slice(scandir($basePath, SCANDIR_SORT_NONE), 2);
+    $paths = array_diff(scandir($basePath), ['.', '..']) ?: [];
     return $withBasePath ? array_map(fn($path) => $basePath . DIRECTORY_SEPARATOR . $path, $paths) : $paths;
 }
 
@@ -47,7 +47,7 @@ function remove_dir(string $dir): bool
     if (is_link($dir) || is_file($dir)) {
         return file_exists($dir) && unlink($dir);
     }
-    $files = array_slice(scandir($dir, SCANDIR_SORT_NONE), 2);
+    $files = array_diff(scandir($dir), ['.', '..']) ?: [];
     foreach ($files as $file) {
         $path = $dir . DIRECTORY_SEPARATOR . $file;
         is_dir($path) && !is_link($path) ? remove_dir($path) : file_exists($path) && unlink($path);

e8fb97b3dec761b5851e9d5148d5c74815207fb6
localzet <creator@localzet.com>
2024-02-09 11:16:55 +0300

    Update

--- config/session.php
+++ config/session.php
@@ -1,11 +1,28 @@
 <?php
 
 /**
- * @package     Triangle Web
- * @link        https://github.com/Triangle-org
+ * @package     Zorin Teashka
+ * @link        https://teashka.zorin.space
+ * @link        https://github.com/localzet-dev/Teashka
  *
- * @copyright   2018-2023 UniT Group
- * @license     https://mit-license.org MIT
+ * @author      Ivan Zorin <creator@localzet.com>
+ * @copyright   Copyright (c) 2018-2024 Zorin Projects S.P.
+ * @license     https://www.gnu.org/licenses/agpl-3.0 GNU Affero General Public License v3.0
+ *
+ *              This program is free software: you can redistribute it and/or modify
+ *              it under the terms of the GNU Affero General Public License as published
+ *              by the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
+ *
+ *              This program is distributed in the hope that it will be useful,
+ *              but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *              MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *              GNU Affero General Public License for more details.
+ *
+ *              You should have received a copy of the GNU Affero General Public License
+ *              along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *              For any questions, please contact <creator@localzet.com>
  */
 
 use Triangle\Engine\Session\FileSessionHandler;
--- support/Request.php
+++ support/Request.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
- * @package     Triangle Engine (FrameX Project)
- * @link        https://github.com/localzet/FrameX      FrameX Project v1-2
- * @link        https://github.com/Triangle-org/Engine  Triangle Engine v2+
+ * @package     Zorin Teashka
+ * @link        https://teashka.zorin.space
+ * @link        https://github.com/localzet-dev/Teashka
  *
  * @author      Ivan Zorin <creator@localzet.com>
- * @copyright   Copyright (c) 2018-2023 UniT Group
- * @license     https://www.gnu.org/licenses/agpl AGPL-3.0 license
+ * @copyright   Copyright (c) 2018-2024 Zorin Projects S.P.
+ * @license     https://www.gnu.org/licenses/agpl-3.0 GNU Affero General Public License v3.0
  *
  *              This program is free software: you can redistribute it and/or modify
- *              it under the terms of the GNU Affero General Public License as
- *              published by the Free Software Foundation, either version 3 of the
- *              License, or (at your option) any later version.
+ *              it under the terms of the GNU Affero General Public License as published
+ *              by the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
  *
  *              This program is distributed in the hope that it will be useful,
  *              but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,6 +21,8 @@
  *
  *              You should have received a copy of the GNU Affero General Public License
  *              along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *              For any questions, please contact <creator@localzet.com>
  */
 
 namespace support;
